512d542e1c7faae916c74371571c8c6d2e48aba9
André Lima <andrealplimapereira10@gmail.com>
2024-10-26 11:03:59 -0300

    Usei os Arrays
    usei os arrays para colocar muitos carros e muitos usuarios.

--- aluguel.c
+++ aluguel.c
@@ -20,3 +20,60 @@ Car cars[MAX_CARS];
 User  users[MAX_USERS];
 int car_count = 0;
 int user_count = 0;
+
+void initializeCars() {
+    strcpy(cars[0].model, "Carro1");
+    cars[0].price_per_day = 50.0;
+    cars[0].available = 1;
+
+    strcpy(cars[1].model, "Carro2");
+    cars[1].price_per_day = 100.0;
+    cars[1].available = 1;
+
+    strcpy(cars[1].model, "Carro3");
+    cars[1].price_per_day = 100.0;
+    cars[1].available = 1;
+
+    strcpy(cars[1].model, "Carro4");
+    cars[1].price_per_day = 100.0;
+    cars[1].available = 1;
+
+    strcpy(cars[1].model, "Carro5");
+    cars[1].price_per_day = 100.0;
+    cars[1].available = 1;
+
+    car_count = 5;
+}
+
+void initializeUsers() {
+    strcpy(users[0].username, "user1");
+    strcpy(users[0].password, "pass1");
+
+    strcpy(users[1].username, "user2");
+    strcpy(users[1].password, "pass2");
+
+    strcpy(users[2].username, "user3");
+    strcpy(users[2].password, "pass3");
+
+    strcpy(users[3].username, "user4");
+    strcpy(users[3].password, "pass4");
+
+    strcpy(users[4].username, "user5");
+    strcpy(users[4].password, "pass5");
+
+    strcpy(users[5].username, "user6");
+    strcpy(users[5].password, "pass6");
+
+    strcpy(users[6].username, "user7");
+    strcpy(users[6].password, "pass7");
+
+    strcpy(users[7].username, "user8");
+    strcpy(users[7].password, "pass8");
+
+    strcpy(users[8].username, "user9");
+    strcpy(users[8].password, "pass9");
+
+    strcpy(users[9].username, "user10");
+    strcpy(users[9].password, "pass10");
+
+    user_count = 10; 
